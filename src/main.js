46f92735b2180dea62e1e387c09f309002255b01
objelisks <objelisks@gmail.com>
2022-03-21 17:51:13 -0700

    cleanup

--- src/main.js
+++ src/main.js
@@ -12,42 +12,6 @@ import { flatShader } from './render/shaders.js'
 // initLoaders()
 loadModel('content/mouse1.gltf').then(model => console.log(model))
 
-// const buildShader = regl
-// const buildModel = regl
-
-// const mouseShader = buildShader({
-//   vert: vertexShader,
-//   frag: fragmentShader
-// })
-// const mouseModel = buildModel({
-//   attributes: {
-//     position: [],
-//     normals: [],
-//     uvs: []
-//   }
-// })
-
-// const miceProps = [
-//   {
-//     position: [0, 0, 0],
-//     rotation: [0, 0, 0]
-//   },
-//   {
-//     position: [1, 0, 0],
-//     rotation: [0, 0, 0]
-//   }
-// ]
-// const drawMice = (props) =>
-//   mouseShader({}, () =>
-//     mouseModel({}, () =>
-//       regl(props)()
-//     )
-//   )
-
-// camera({ eye: [10, 0, 10], target: [0, 0, 0] }, () => {
-//   drawMice(miceProps)
-// })
-
 let tick = 0
 
 const draw = () => {
